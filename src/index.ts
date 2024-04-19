89aa039f1f10f130037ac55855e611bea6a3e0af
Jakub Slys <jakub@slys.dev>
2024-04-19 15:47:33 +0200

    force reinstall

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,7 @@ export const build = async ({
 
   // install poetry
   console.log('installing setuptools and poetry')
-  await pipInstall(pythonVersion.pipPath, workPath, ["-U",  "urllib3<=1.26.15"]);
+  await pipInstall(pythonVersion.pipPath, workPath, ["-U", "-vvv", "--force-reinstall",  "urllib3<=1.26.15"]);
   await pipInstall(pythonVersion.pipPath, workPath, ["-U",  "setuptools"]);
   await pipInstall(pythonVersion.pipPath, workPath, ["-U",  "poetry"]);
 
