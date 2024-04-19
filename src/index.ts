e6325204f923a3c4679deb279b7c5a83bfea1a2a
Jakub Slys <jakub@slys.dev>
2024-04-19 14:42:08 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,176 @@
+import { join, dirname, basename } from 'path';
+import execa from 'execa';
+import fs from 'fs';
+import { promisify } from 'util';
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+import {
+  GlobOptions,
+  BuildOptions,
+  download,
+  glob,
+  createLambda,
+  shouldServe,
+  debug,
+} from '@vercel/build-utils';
+import { installRequirement, installRequirementsFile, pipInstall } from './install';
+import { getLatestPythonVersion } from './version';
+
+export const version = 3;
+
+export async function downloadFilesInWorkPath({
+  entrypoint,
+  workPath,
+  files,
+  meta = {},
+}: Pick<BuildOptions, 'entrypoint' | 'workPath' | 'files' | 'meta'>) {
+  debug('Downloading user files...');
+  let downloadedFiles = await download(files, workPath, meta);
+  if (meta.isDev) {
+    // Old versions of the CLI don't assign this property
+    const { devCacheDir = join(workPath, '.now', 'cache') } = meta;
+    const destCache = join(devCacheDir, basename(entrypoint, '.py'));
+    await download(downloadedFiles, destCache);
+    downloadedFiles = await glob('**', destCache);
+    workPath = destCache;
+  }
+  return workPath;
+}
+
+export const build = async ({
+  workPath,
+  files: originalFiles,
+  entrypoint,
+  meta = {},
+  config,
+}: BuildOptions) => {
+  let pythonVersion = getLatestPythonVersion(meta);
+  workPath = await downloadFilesInWorkPath({
+    workPath,
+    files: originalFiles,
+    entrypoint,
+    meta,
+  });
+
+  // install poetry
+  console.log('installing setuptools and poetry')
+  await pipInstall(pythonVersion.pipPath, workPath, ["-U",  "setuptools"]);
+  await pipInstall(pythonVersion.pipPath, workPath, ["-U",  "poetry"]);
+
+  const cmdArgs = [
+    "-m",
+    "poetry",
+    "export",
+    "--without-hashes",
+    "-f",
+    "requirements.txt",
+    "--output",
+    "requirements.txt",
+  ];
+  // Export requirements.txt file before installation of requirements file
+  await execa(pythonVersion.pythonPath, cmdArgs, { cwd: workPath });
+  console.log(`Successfully written requirement.txt file`)
+
+  try {
+    // See: https://stackoverflow.com/a/44728772/376773
+    //
+    // The `setup.cfg` is required for `vercel dev` on MacOS, where without
+    // this file being present in the src dir then this error happens:
+    //
+    // distutils.errors.DistutilsOptionError: must supply either home
+    // or prefix/exec-prefix -- not both
+    if (meta.isDev) {
+      const setupCfg = join(workPath, 'setup.cfg');
+      await writeFile(setupCfg, '[install]\nprefix=\n');
+    }
+  } catch (err) {
+    console.log('Failed to create "setup.cfg" file');
+    throw err;
+  }
+
+  console.log('Installing required dependencies...');
+
+  await installRequirement({
+    pythonPath: pythonVersion.pythonPath,
+    pipPath: pythonVersion.pipPath,
+    dependency: 'werkzeug',
+    version: '1.0.1',
+    workPath,
+    meta,
+  });
+
+  let fsFiles = await glob('**', workPath);
+  const entryDirectory = dirname(entrypoint);
+
+  fsFiles = await glob('**', workPath);
+  const requirementsTxt = join(entryDirectory, 'requirements.txt');
+
+  if (fsFiles[requirementsTxt]) {
+    debug('Found local "requirements.txt"');
+    const requirementsTxtPath = fsFiles[requirementsTxt].fsPath;
+    await installRequirementsFile({
+      pythonPath: pythonVersion.pythonPath,
+      pipPath: pythonVersion.pipPath,
+      filePath: requirementsTxtPath,
+      workPath,
+      meta,
+    });
+  } else if (fsFiles['requirements.txt']) {
+    debug('Found global "requirements.txt"');
+    const requirementsTxtPath = fsFiles['requirements.txt'].fsPath;
+    await installRequirementsFile({
+      pythonPath: pythonVersion.pythonPath,
+      pipPath: pythonVersion.pipPath,
+      filePath: requirementsTxtPath,
+      workPath,
+      meta,
+    });
+  }
+
+  // After installation of requirements.txt file finished, run the buildCommand if specified
+  if (config.buildCommand) {
+    console.log(`Running ${pythonVersion.pythonPath} ${config.buildCommand}`);
+    const cmdArgs = config.buildCommand.split(' ');
+    await execa(pythonVersion.pythonPath, cmdArgs, { cwd: workPath });
+  }
+
+  const originalPyPath = join(__dirname, '..', 'vc_init.py');
+  const originalHandlerPyContents = await readFile(originalPyPath, 'utf8');
+  debug('Entrypoint is', entrypoint);
+  const moduleName = entrypoint.replace(/\//g, '.').replace(/\.py$/, '');
+  // Since `vercel dev` renames source files, we must reference the original
+  const suffix = meta.isDev && !entrypoint.endsWith('.py') ? '.py' : '';
+  const entrypointWithSuffix = `${entrypoint}${suffix}`;
+  debug('Entrypoint with suffix is', entrypointWithSuffix);
+  const handlerPyContents = originalHandlerPyContents
+    .replace(/__VC_HANDLER_MODULE_NAME/g, moduleName)
+    .replace(/__VC_HANDLER_ENTRYPOINT/g, entrypointWithSuffix);
+
+  // in order to allow the user to have `server.py`, we need our `server.py` to be called
+  // somethig else
+  const handlerPyFilename = 'vc__handler__python';
+
+  await writeFile(join(workPath, `${handlerPyFilename}.py`), handlerPyContents);
+
+  const globOptions: GlobOptions = {
+    cwd: workPath,
+    ignore:
+      config && typeof config.excludeFiles === 'string'
+        ? config.excludeFiles
+        : 'node_modules/**',
+  };
+
+  const lambda = await createLambda({
+    files: await glob('**', globOptions),
+    handler: `${handlerPyFilename}.vc_handler`,
+    runtime: pythonVersion.runtime,
+    environment: {},
+  });
+
+  return { output: lambda };
+};
+
+export { shouldServe };
+
+// internal only - expect breaking changes if other packages depend on these exports
+export { installRequirement, installRequirementsFile };
